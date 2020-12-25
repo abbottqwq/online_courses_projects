ae099cc36cb71dd3183fb15d6cc2da06e4947914
lizhuohui <li.zhuohui@northeastern.edu>
2020-12-25 03:13:19 -0500

    day1 update

--- dragon_stack/backend/dragon.js
+++ dragon_stack/backend/dragon.js
@@ -1,3 +1,14 @@
-class Dragon {
-
+const DEFAULT_PROPERTIES = {
+    nickname: 'unnamed',
+    get birthday() {
+        return new Date();
+    }
 }
+class Dragon {
+    constructor({birthday, nickname} = {}) {
+        this.birthday = birthday || DEFAULT_PROPERTIES.birthday;
+        this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
+    }
+}
+
+module.exports = Dragon;
---
+++ dragon_stack/backend/index.js
@@ -0,0 +1,7 @@
+const Dragon = require('./dragon.js')
+const fooey = new Dragon({
+    nickname: 'fooey'
+});
+
+const fooey2 = new Dragon()
+console.log(fooey)
