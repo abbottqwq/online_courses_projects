30dd4a2bb3a03b75cc9f500a28f7c79fed93c057
lizhuohui <li.zhuohui@northeastern.edu>
2020-12-27 06:08:20 -0500

    section3

--- dragon_stack/backend/app/index.js
+++ dragon_stack/backend/app/index.js
@@ -1,16 +1,18 @@
 const GenerationEngine = require('./generation/engine.js');
 const {SECONDS} = require('./config.js');
 const express = require('express');
+const dragonRouter = require('./api/dragon.js')
+const generationRouter = require('./api/generation.js');
 
 const app = express();
 const engine = new GenerationEngine();
 
+app.locals.engine = engine;
 
+app.use('/dragon', dragonRouter);
+app.use('/generation', generationRouter);
 engine.start();
 
-app.get('/dragon/new', (req, res) => {
-    res.json({dragon: engine.generation.newDragon()})
-});
 
 
 
