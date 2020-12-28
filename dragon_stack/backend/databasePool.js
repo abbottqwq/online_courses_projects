7493312fc649345e3f2082293c90bc20968e4c29
lizhuohui <li.zhuohui@northeastern.edu>
2020-12-28 07:31:05 -0500

    sql

--- dragon_stack/backend/databasePool.js
+++ dragon_stack/backend/databasePool.js
@@ -4,3 +4,9 @@ const config = require('./secret/databaseConfig.js')
 const pool = new Pool(config);
 
 module.exports = pool;
+
+
+// pool.query('select * from generation', (err, result) => {
+//     if (err) return console.log('Error', err);
+//     console.log(`result.rows `, result.rows);
+// })
