6340edfb0d5d3a0f7ce426c4044d689e79c603fb
abbottqwq <71251245+abbottqwq@users.noreply.github.com>
2021-01-01 06:19:12 -0500

    muti-system bash script

--- dragon_stack/backend/bin/configure_db.sh
+++ dragon_stack/backend/bin/configure_db.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 export PGPASSWORD='node_password'
 
@@ -7,7 +7,25 @@ echo "configuring dragonstackdb"
 dropdb -U node_user dragonstackdb
 createdb -U node_user dragonstackdb
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
+
+if [ ${machine}=="MinGw" ] || [ ${machine}=="Cygwin" ]; 
+then
+psql.exe -U node_user dragonstackdb <./bin/sql/generation.sql
+psql.exe -U node_user dragonstackdb <./bin/sql/dragon.sql
+$SHELL
+elif [ ${machine}=="Mac" ] || [ ${machine}=="Linux" ]; then
 psql -U node_user dragonstackdb <./bin/sql/generation.sql
 psql -U node_user dragonstackdb <./bin/sql/dragon.sql
+fi
+
 
 echo "dragonstackdb configure"
